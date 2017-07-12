087981d6ed77824736af6337b05bb97c86059c3d
romklao <rk120879@gmail.com>
2017-07-12 15:01:42 -0700

    Change whatwedo font size responsive

--- js/components/whatWeDoBox.js
+++ js/components/whatWeDoBox.js
@@ -9,17 +9,17 @@ import LoginSignup from './loginSignup';
 export function WhatWeDoBox(props) {
     let showSignup = event => {
         event.preventDefault();
-        props.dispatch(actions.showSignup());        
+        props.dispatch(actions.showSignup());
     };
 
     let showLogin = event => {
         event.preventDefault();
-        props.dispatch(actions.showLogin());        
+        props.dispatch(actions.showLogin());
     };
 
     return (
         <div className="row whatWeDo">
-            <div className="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div className="explainBox">
                     <p className="explain">Search for products, sign up to save your favorite items and share with your friends</p>
                     <button className="btn btn-default loginBtn" onClick={showLogin}>Log In</button>
