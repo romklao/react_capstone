e6fd660183c34041bc941142a935985ddb1c9391
romklao <rk120879@gmail.com>
2017-07-18 18:14:27 -0700

    Added ShowLoginSignup.js

--- js/components/whatWeDoBox.js
+++ js/components/whatWeDoBox.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import * as actions from '../actions/index';
 import LoginModal from './LoginModal';
 import SignupModal from './SignupModal';
-import LoginSignup from './LoginSignup';
+import ShowLoginSignUp from './ShowLoginSignUp';
 
 export function WhatWeDoBox(props) {
     let showSignup = event => {
