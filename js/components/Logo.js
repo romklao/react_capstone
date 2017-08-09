7a7cf910bd3cef586c3a674288ca0c155f28331b
romklao <rk120879@gmail.com>
2017-08-08 22:11:38 -0700

    Added toggle navbar mobile

--- js/components/Logo.js
+++ js/components/Logo.js
@@ -13,6 +13,12 @@ export function Logo(props) {
     }
     return (
         <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+            </button>
             <a className="navbar-brand" id="homeIcon" href="/"><img src="css/images/home.png" width="21px"/></a>
             <a className="navbar-brand" href="#" onClick={returnHome}>HomeDecor</a>
         </div>
